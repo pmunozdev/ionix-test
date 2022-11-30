db46beed425ab189be85492ce5d4c15a0bae5667
Pablo Munoz <pmunozdev@gmail.com>
2022-11-30 10:32:13 -0300

    commit inicial

---
+++ src/main/java/com/ionix/test/backend/Application.java
@@ -0,0 +1,13 @@
+package com.ionix.test.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
